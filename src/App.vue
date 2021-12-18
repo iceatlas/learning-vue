1242d1b03bcf769bcf882f3eafa817c32cfefe95
iceatlas <xbzhang2020@icloud.com>
2021-12-18 18:32:07 +0800

    feat: tsx测试

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div id="app">
     <TsxTest />
+    <div v-for="(item, index) in list" :key="index">
+      <component :is="item"/>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import TsxTest from '@/components/TsxTest'
+import TsxTest, { List } from '@/components/TsxTest'
 
 @Component({
   components: {
     TsxTest,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  list = List
+}
 </script>
 
 <style lang="scss"></style>
--- src/components/TsxTest.tsx
+++ src/components/TsxTest.tsx
@@ -2,7 +2,20 @@ import { Component, Vue } from 'vue-property-decorator'
 
 @Component({})
 export default class Test extends Vue {
-  render(){
+  render() {
     return <div>tsx测试</div>
   }
 }
+
+export const List = [
+  {
+    render() {
+      return <p>1</p>
+    },
+  },
+  {
+    render() {
+      return <p>2</p>
+    },
+  },
+]
