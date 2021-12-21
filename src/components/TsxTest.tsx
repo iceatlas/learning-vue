2bf6e580edafbdac39b4e09ec65bfc4efd00bf8d
zhangxingbin <zhangxingbin@meituan.com>
2021-12-21 10:31:12 +0800

    feat: jsx

--- src/components/TsxTest.tsx
+++ src/components/TsxTest.tsx
@@ -1,9 +1,42 @@
 import { Component, Vue } from 'vue-property-decorator'
 
+Vue.component('anchored-heading', {
+  render: function (createElement) {
+    return createElement(
+      'h' + this.level, // 标签名称
+      this.$slots.default // 子节点数组
+    )
+  },
+  props: {
+    level: {
+      type: Number,
+      required: true,
+    },
+  },
+})
+
+Vue.component('anchored-jsx-heading', {
+  render() {
+    const Cmp = `h${this.level}`
+    return <Cmp>{this.$slots.default}</Cmp>
+  },
+  props: {
+    level: {
+      type: Number,
+      required: true,
+    },
+  },
+})
+
 @Component({})
 export default class Test extends Vue {
   render() {
-    return <div>tsx测试</div>
+    return (
+      <div>
+        <anchored-heading level={1}>Hello world!</anchored-heading>
+        <anchored-jsx-heading level={2}>Hello world!111</anchored-jsx-heading>
+      </div>
+    )
   }
 }
 
